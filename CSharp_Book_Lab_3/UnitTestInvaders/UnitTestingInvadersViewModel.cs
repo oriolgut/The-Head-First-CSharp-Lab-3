416542c51f5a2c5f746107213f31f310e704944c
OriolG <download.spam.net@gmail.com>
2016-10-25 13:28:45 +0200

    added class for testign the InvadersViewModel class

---
+++ CSharp_Book_Lab_3/UnitTestInvaders/UnitTestingInvadersViewModel.cs
@@ -0,0 +1,22 @@
+﻿using InvadersWindowsStoreApp.ViewModel;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework.AppContainer;
+
+namespace UnitTestInvaders
+{
+    [TestClass]
+    public class UnitTestingInvadersViewModel
+    {
+        [UITestMethod]
+        public void TestRecreateScanLines()
+        {
+            InvadersViewModel viewModel = new InvadersViewModel();
+            int oldScanLines = viewModel.ScanLines.Count;
+            viewModel.RecreateScanLines();
+            int newScanLines = viewModel.ScanLines.Count;
+
+            Microsoft.VisualStudio.TestPlatform.UnitTestFramework.Assert.AreNotEqual(oldScanLines, newScanLines, "Should not be equal. \"newScanLines\" should contain Scan Lines.");
+        }
+
+    }
+}
